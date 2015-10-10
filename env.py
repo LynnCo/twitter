5fc7ee9ac8a3482f7df02d96dedb86f638579a12
Lynn Cyrin <firemagelynn@gmail.com>
2015-10-10 14:13:08 -0700

    clarify env files

--- env.py
+++ env.py
@@ -6,7 +6,9 @@ dotenv.load_dotenv('.env')
 ENV = {
     'API_KEY':      os.environ['API_KEY'],
     'API_SECRET':   os.environ['API_SECRET'],
+    'ACCESS_TOKEN': os.environ['ACCESS_TOKEN'], # user context
+    'TOKEN_SECRET': os.environ['TOKEN_SECRET'], # user context
     'ROOT':         os.environ['ROOT'],
-    'PORT':         os.environ['PORT'],
+    'PORT':         os.environ['PORT'],         # local only
     'SECRET_KEY':   os.environ['SECRET_KEY'],
 }
